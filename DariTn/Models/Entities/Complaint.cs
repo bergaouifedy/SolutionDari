2ca7bd0e92e032e8c65aa67471151d587f6cc970
selimchikhzaouali <42379771+selimchikhzaouali@users.noreply.github.com>
2021-04-20 15:27:09 +0100

    entités media+complaint

---
+++ DariTn/Models/Entities/Complaint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace DariTN.Models.Entities
+{
+    public class Complaint
+    {
+
+        public int id { get; set; }
+        public string @ref { get; set; }
+        public string description { get; set; }
+        public bool status { get; set; }
+        public DateTime creationDate { get; set; }
+       // [ForeignKey("AssetAdv")]
+      //  public List<AssetAdv> asset { get; set; }
+      //  [ForeignKey("User")]
+       // public List<User> user { get; set; }
+    }
+}
---
+++ DariTn/Models/Entities/Media.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace DariTN.Models.Entities
+{
+    public class Media
+    {
+        public int id { get; set; }
+        public string @ref { get; set; }
+        public string title { get; set; }
+        public string type { get; set; }
+       // [ForeignKey("AssetAdv")]
+       // public List<AssetAdv> assetAdv { get; set; }
+        public byte[] file { get; set; }
+}
+}
