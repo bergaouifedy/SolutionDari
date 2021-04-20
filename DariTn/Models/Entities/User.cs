d9925be2db0c8ccbb1c8d0d1b27b06da404267aa
wissalwertani <wertani.wissal@esprit.tn>
2021-04-20 22:11:31 +0100

    TEST1

---
+++ DariTn/Models/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DariTn.Models.Entities
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+        public string phoneNumber { get; set; }
+        public string postalCode { get; set; }
+        public object role { get; set; }
+        public object searchHistory { get; set; }
+        public List<object> asset { get; set; }
+        public List<object> complaint { get; set; }
+        public List<object> boughtassets { get; set; }
+    }
+}
